d2e58bd721d250cb3652a9352c22e662e1a97b91
Matthew Hillman <mhillman2@aol.com>
2024-01-12 20:28:02 +0000

    Add bootloader file with FAT12 header

--- build.bat
+++ build.bat
@@ -1,3 +1,8 @@
 mkdir build
-nasm src/main.asm -f bin -o build/main.bin
-cp build/main.bin build/main.img
+mkdir "build/kernel"
+mkdir "build/bootloader"
+
+nasm src/bootloader/main.asm -f bin -o build/bootloader/main.bin
+nasm src/kernel/main.asm -f bin -o build/kernel/main.bin
+
+"./tools/fat_imgen" -c -F -f build/main.img -s build/bootloader/main.bin

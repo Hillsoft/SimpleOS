d98588492eaf25a382cd67a6a1ef41bebd89314b
Matthew Hillman <mhillman2@aol.com>
2024-01-12 22:30:46 +0000

    Add a test file into the disc image

--- build.bat
+++ build.bat
@@ -5,4 +5,4 @@ mkdir "build/bootloader"
 nasm src/bootloader/main.asm -f bin -o build/bootloader/main.bin
 nasm src/kernel/main.asm -f bin -o build/kernel/main.bin
 
-"./tools/fat_imgen" -c -F -f build/main.img -s build/bootloader/main.bin
+"./tools/fat_imgen" -c -F -f build/main.img -s build/bootloader/main.bin -i res/test.txt -n test.txt

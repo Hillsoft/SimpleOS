37f7170a4db666167809f49a04d3f91d119c1a08
Matthew Hillman <mhillman2@aol.com>
2024-01-13 17:09:06 +0000

    Cleanup build script to make adding more files easier

--- build.bat
+++ build.bat
@@ -5,4 +5,5 @@ mkdir "build/bootloader"
 nasm src/bootloader/main.asm -f bin -o build/bootloader/main.bin
 nasm src/kernel/main.asm -f bin -o build/kernel/main.bin
 
-"./tools/fat_imgen" -c -F -f build/main.img -s build/bootloader/main.bin -i res/test.txt -n test.txt
+"./tools/fat_imgen" -c -F -f build/main.img -s build/bootloader/main.bin
+"./tools/fat_imgen" -m -f build/main.img -i res/test.txt -n test.txt

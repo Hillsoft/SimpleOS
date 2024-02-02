3c74ba5f74125283d2f86857eb4b20161934602f
Matthew Hillman <mhillman2@aol.com>
2024-02-02 21:03:26 +0000

    Add a few more memory utility methods

--- src/bootloader/stage2/string.asm
+++ src/bootloader/stage2/string.asm
@@ -172,3 +172,173 @@ memset_far:
   mov sp, bp
   pop bp
   ret
+
+global memcpy
+; void memcpy(char* dst, const char* src, uint16_t count)
+memcpy:
+  ; new call frame
+  push bp
+  mov bp, sp
+
+  push di
+  push si
+
+  ; [bp + 0] - old bp
+  ; [bp + 2] - return address
+  ; [bp + 4] - dst
+  ; [bp + 6] - src
+  ; [bp + 8] - count
+
+  mov cx, [bp + 8]
+  mov di, [bp + 4]
+  mov si, [bp + 6]
+
+.loop:
+  or cx, cx
+  jz .finish
+
+  mov ax, [si]
+  mov [di], ax
+  inc si
+  inc di
+  dec cx
+  jmp .loop
+
+.finish:
+  ; return
+  pop si
+  pop di
+
+  mov sp, bp
+  pop bp
+  ret
+
+global strlen
+; uint16_t strlen(const char* str)
+strlen:
+  ; new call frame
+  push bp
+  mov bp, sp
+
+  push si
+
+  ; [bp + 0] - old bp
+  ; [bp + 2] - return address
+  ; [bp + 4] - str
+
+  xor cx, cx
+  mov si, [bp + 4]
+.loop:
+  lodsb
+  or ax, ax
+  jz .finish
+
+  inc cx
+  jmp .loop
+
+.finish:
+  mov ax, cx
+
+  ; return
+  pop si
+
+  mov sp, bp
+  pop bp
+  ret
+
+global memeq_far
+; bool memeq(const char far* a, const char far* b, uint16_t count)
+memeq_far:
+  ; new call frame
+  push bp
+  mov bp, sp
+
+  push es
+  push fs
+  push di
+  push si
+
+  ; [bp + 0] - old bp
+  ; [bp + 2] - return address
+  ; [bp + 4] - a
+  ; [bp + 8] - b
+  ; [bp + 12] - count
+
+  mov di, [bp + 4]
+  mov es, [bp + 6]
+  mov si, [bp + 8]
+  mov fs, [bp + 10]
+  mov cx, [bp + 12]
+
+.loop:
+  or cx, cx
+  mov ax, 1
+  jz .finish
+
+  mov ax, es:[di]
+  mov dx, fs:[si]
+  cmp ax, dx
+  jne .finish_ne
+
+  dec cx
+  inc si
+  inc di
+  jmp .loop
+
+.finish_ne:
+  mov ax, 0
+
+.finish:
+  ; return
+  pop si
+  pop di
+  pop fs
+  pop es
+
+  mov sp, bp
+  pop bp
+  ret
+
+global streq
+; bool streq(const char* a, const char* b)
+streq:
+  ; new call frame
+  push bp
+  mov bp, sp
+
+  push di
+  push si
+
+  ; [bp + 0] - old bp
+  ; [bp + 2] - return address
+  ; [bp + 4] - a
+  ; [bp + 6] - b
+
+  mov di, [bp + 4]
+  mov si, [bp + 6]
+
+.loop:
+  mov ax, [di]
+  mov cx, [si]
+  cmp ax, cx
+  jne .finish_ne
+
+  or ax, ax
+  mov ax, 1
+  jz .finish
+
+  inc si
+  inc di
+  jmp .loop
+
+.finish_ne:
+  mov ax, 0
+
+.finish:
+  ; return
+  pop si
+  pop di
+
+  mov sp, bp
+  pop bp
+  ret

2b2e58eeabd31a817196eb33f11dc540dd0de544
Matthew Hillman <mhillman2@aol.com>
2024-02-04 21:46:21 +0000

    Enable O3 and LTO

--- src/bootloader/stage3/main.cpp
+++ src/bootloader/stage3/main.cpp
@@ -7,7 +7,7 @@ extern "C" {
 void __attribute__((cdecl)) cstart(uint8_t bootDrive) {
   mysty::clrscr();
   mysty::puts("Hello, from cstart!\n");
-  mysty::printf("Percent: %%\nChar: %c\nInt: %u\nString: %s", 'c', 543, "test string");
+  mysty::printf("Percent: %%\nChar: %c\nInt: %u\nString: %s\n", 'c', 543, "test string");
 }
 
 }

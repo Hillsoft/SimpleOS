16999f90128d65fe899f7c7b0bcd4af92772a299
Matthew Hillman <mhillman2@aol.com>
2024-02-05 20:22:08 +0000

    Correct return type for `x86_inb`.

--- src/bootloader/stage3/x86.hpp
+++ src/bootloader/stage3/x86.hpp
@@ -5,6 +5,6 @@
 extern "C" {
 
 void __attribute__((cdecl)) x86_outb(uint16_t port, uint8_t value);
-void __attribute__((cdecl)) x86_inb(uint16_t port);
+uint8_t __attribute__((cdecl)) x86_inb(uint16_t port);
 
 }

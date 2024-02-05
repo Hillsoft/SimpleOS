c6b819abd1b269b215d5bad9345fadab57b19d16
Matthew Hillman <mhillman2@aol.com>
2024-02-05 20:23:46 +0000

    Begin work on a basic floppy disk driver

---
+++ src/bootloader/stage3/disk.cpp
@@ -0,0 +1,255 @@
+#include "disk.hpp"
+
+#include "mystyarray.hpp"
+#include "mystyio.hpp"
+#include "mystyoptional.hpp"
+#include "x86.hpp"
+
+namespace simpleos::disk {
+
+namespace {
+
+constinit bool g_hasConfiguredDriveController = false;
+constinit uint8_t g_currentDrive = 0;
+
+constexpr uint32_t kHeadsPerCylinder = 2;
+constexpr uint32_t kSectorsPerTrack = 18;
+
+struct CHS {
+  uint16_t cylinder;
+  uint16_t head;
+  uint16_t sector;
+
+  static constexpr CHS fromLBA(uint32_t lba) {
+    return CHS{
+        .cylinder =
+            static_cast<uint16_t>((lba / kSectorsPerTrack) / kHeadsPerCylinder),
+        .head =
+            static_cast<uint16_t>((lba / kSectorsPerTrack) % kHeadsPerCylinder),
+        .sector = static_cast<uint16_t>((lba % kSectorsPerTrack) + 1)};
+  }
+};
+
+enum FloppyRegister : uint16_t {
+  STATUS_REGISTER_A = 0x3F0, // read-only
+  STATUS_REGISTER_B = 0x3F1, // read-only
+  DIGITAL_OUTPUT_REGISTER = 0x3F2,
+  TAPE_DRIVE_REGISTER = 0x3F3,
+  MAIN_STATUS_REGISTER = 0x3F4, // read-only
+  DATARATE_SELECT_REGISTER = 0x3F4, // write-only
+  DATA_FIFO = 0x3F5,
+  DIGITAL_INPUT_REGISTER = 0x3F7, // read-only
+  CONFIGURATION_CONTROL_REGISTER = 0x3F7, // write-only
+};
+
+enum FloppyCommand : uint8_t {
+  READ_TRACK = 2,
+  SPECIFY = 3,
+  SENSE_DRIVE_STATUS = 4,
+  WRITE_DATA = 5,
+  READ_DATA = 6,
+  RECALIBRATE = 7,
+  SENSE_INTERRUPT = 8,
+  WRITE_DELETED_DATA = 9,
+  READ_ID = 10,
+  READ_DELETED_DATA = 12,
+  FORMAT_TRACK = 13,
+  DUMPREG = 14,
+  SEEK = 15,
+  VERSION = 16,
+  SCAN_EQUAL = 17,
+  PERPENDICULAR_MODE = 18,
+  CONFIGURE = 19,
+  LOCK = 20,
+  VERIFY = 22,
+  SCAN_LOW_OR_EQUAL = 25,
+  SCAN_HIGH_OR_EQUAL = 29,
+};
+
+class MainStatusRegisterBitset {
+ public:
+  explicit MainStatusRegisterBitset(uint8_t bitset) : bitset_(bitset) {}
+
+  static MainStatusRegisterBitset loadFromController() {
+    return MainStatusRegisterBitset{
+        x86_inb(FloppyRegister::MAIN_STATUS_REGISTER)};
+  }
+
+  constexpr bool getRQM() const { return (bitset_ & 0x80) > 0; }
+
+  constexpr bool getDIO() const { return (bitset_ & 0x40) > 0; }
+
+  constexpr bool getNDMA() const { return (bitset_ & 0x20) > 0; }
+
+  constexpr bool getCB() const { return (bitset_ & 0x10) > 0; }
+
+  constexpr bool isDriveSeeking(uint8_t driveNumber) {
+    return (bitset_ & (0x1 << driveNumber)) > 0;
+  }
+
+ private:
+  uint8_t bitset_;
+};
+
+void selectDrive(uint8_t driveNumber) {
+  x86_outb(FloppyRegister::CONFIGURATION_CONTROL_REGISTER, 0);
+
+  uint8_t motorOnBit = 0x10 << driveNumber;
+  uint8_t setBit = 0x4;
+  uint8_t selectBits = driveNumber;
+
+  x86_outb(
+      FloppyRegister::DIGITAL_OUTPUT_REGISTER,
+      motorOnBit | setBit | selectBits);
+}
+
+void resetDriveController() {
+  x86_outb(FloppyRegister::DATARATE_SELECT_REGISTER, 0x80);
+
+  // Assume a LOCK command has been issued earlier
+
+  selectDrive(g_currentDrive);
+}
+
+template <
+    uint32_t inputParameterCount,
+    uint32_t outputByteCount,
+    uint32_t bufferSize>
+bool executeDriveControllerCommandSingleAttempt(
+    FloppyCommand command,
+    const mysty::FixedArray<uint8_t, inputParameterCount>& inputParameters,
+    mysty::FixedArray<uint8_t, outputByteCount>& outBytes,
+    mysty::FixedArray<uint8_t, bufferSize>& buffer) {
+  MainStatusRegisterBitset status =
+      MainStatusRegisterBitset::loadFromController();
+
+  if (!status.getRQM() || status.getDIO()) {
+    resetDriveController();
+    return false;
+  }
+
+  x86_outb(FloppyRegister::DATA_FIFO, command);
+
+  for (const auto& parameter : inputParameters) {
+    // Wait until ready
+    while (status = MainStatusRegisterBitset::loadFromController(),
+           !status.getRQM()) {
+    }
+    if (status.getDIO()) {
+      return false;
+    }
+
+    x86_outb(FloppyRegister::DATA_FIFO, parameter);
+  }
+
+  // Check if the current command has an execution phase
+  status = MainStatusRegisterBitset::loadFromController();
+  auto bufferIt = buffer.begin();
+  while (status.getNDMA()) {
+    // We do have an execution phase
+    while (status = MainStatusRegisterBitset::loadFromController(),
+           !status.getRQM()) {
+    }
+
+    while (status = MainStatusRegisterBitset::loadFromController(),
+           status.getRQM() && status.getNDMA()) {
+    }
+
+    if (bufferIt == buffer.end()) {
+      return false;
+    }
+
+    *bufferIt = x86_inb(FloppyRegister::DATA_FIFO);
+    ++bufferIt;
+  }
+
+  // Result phase
+  while (status = MainStatusRegisterBitset::loadFromController(),
+         !status.getRQM()) {
+  }
+  if (!status.getDIO()) {
+    return false;
+  }
+
+  bool first = true;
+  for (auto& outByte : outBytes) {
+    while (status = MainStatusRegisterBitset::loadFromController(),
+           !first && status.getRQM()) {
+    }
+    if (!status.getDIO() || !status.getCB()) {
+      return false;
+    }
+    outByte = x86_inb(FloppyRegister::DATA_FIFO);
+    first = false;
+  }
+
+  status = MainStatusRegisterBitset::loadFromController();
+  if (!status.getRQM() || status.getCB() || status.getDIO()) {
+    return false;
+  }
+
+  return true;
+}
+
+template <
+    uint32_t inputParameterCount,
+    uint32_t outputByteCount,
+    uint32_t bufferSize>
+bool executeDriveControllerCommand(
+    FloppyCommand command,
+    const mysty::FixedArray<uint8_t, inputParameterCount>& inputParameters,
+    mysty::FixedArray<uint8_t, outputByteCount>& outBytes,
+    mysty::FixedArray<uint8_t, bufferSize>& buffer) {
+  // Try up to three times
+  for (int i = 0; i < 3; i++) {
+    if (executeDriveControllerCommandSingleAttempt(
+            command, inputParameters, outBytes, buffer)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+mysty::Optional<uint8_t> getDriveControllerVersion() {
+  mysty::FixedArray<uint8_t, 0> input;
+  mysty::FixedArray<uint8_t, 1> output;
+  mysty::FixedArray<uint8_t, 0> buffer;
+
+  bool success = executeDriveControllerCommand(
+      FloppyCommand::VERSION, input, output, buffer);
+
+  if (success) {
+    return output[0];
+  }
+
+  return mysty::Optional<uint8_t>{};
+}
+
+void configureDriveController(
+    bool impliedSeekEnable,
+    bool fifoEnable,
+    bool pollingEnable,
+    uint8_t threshold,
+    uint8_t precompensation) {
+  x86_outb(FloppyRegister::DATA_FIFO, FloppyCommand::CONFIGURE);
+}
+
+} // namespace
+
+bool initialize(uint8_t driveNumber) {
+  mysty::Optional<uint8_t> version = getDriveControllerVersion();
+  if (!version.has_value()) {
+    mysty::puts("Failed to get floppy controller version\n");
+    return false;
+  }
+  if (*version != 0x90) {
+    mysty::puts("Unsupported floppy controller\n");
+    return false;
+  }
+
+  // Not implemented
+  return false;
+}
+
+} // namespace simpleos::disk
---
+++ src/bootloader/stage3/disk.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "mystyint.hpp"
+
+namespace simpleos::disk {
+
+bool initialize(uint8_t driveNumber);
+
+} // namespace simpleos::disk

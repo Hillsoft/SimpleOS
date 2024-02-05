2dcb164370f8a9f00507a3c9e95e8e2fad655a57
Matthew Hillman <mhillman2@aol.com>
2024-02-05 20:24:26 +0000

    Attempt to initialise floppy drive in main. This will fail for now because initialisation is not yet fully implemented.

--- src/bootloader/stage3/main.cpp
+++ src/bootloader/stage3/main.cpp
@@ -1,4 +1,6 @@
 #include "main.hpp"
+
+#include "disk.hpp"
 #include "mystyint.hpp"
 #include "mystyio.hpp"
 
@@ -7,6 +9,10 @@ extern "C" {
 void __attribute__((cdecl)) cstart(uint8_t bootDrive) {
   mysty::clrscr();
   mysty::puts("Hello, from cstart!\n");
+  if (!simpleos::disk::initialize(bootDrive)) {
+    mysty::printf("Failed to initialize floppy driver using disk %u\n", bootDrive);
+    return;
+  }
   mysty::printf("Percent: %%\nChar: %c\nInt: %u\nString: %s\n", 'c', 543, "test string");
 }
 

4580346f1a08ccec9c0365d2e0491910662e825f
Matthew Hillman <mhillman2@aol.com>
2024-02-07 18:29:57 +0000

    Add constexpr annotations to array

--- src/bootloader/stage3/mysty/array.hpp
+++ src/bootloader/stage3/mysty/array.hpp
@@ -15,15 +15,16 @@ class FixedArray {
    public:
     using value_t = IT;
 
-    Iterator(IArr& array, size_t index) : array_(array), index_(index) {}
+    constexpr Iterator(IArr& array, size_t index)
+        : array_(array), index_(index) {}
 
-    bool operator==(Iterator const& other) const {
+    constexpr bool operator==(Iterator const& other) const {
       return &array_ == &other.array_ && index_ == other.index_;
     }
 
-    IT& operator*() const { return array_[index_]; }
+    constexpr IT& operator*() const { return array_[index_]; }
 
-    Iterator const& operator++() {
+    constexpr Iterator const& operator++() {
       index_ += 1;
       return *this;
     }
@@ -37,39 +38,45 @@ class FixedArray {
   using iterator_t = Iterator<T, FixedArray>;
   using const_iterator_t = Iterator<const T, const FixedArray>;
 
-  FixedArray() {}
+  constexpr FixedArray() {}
 
-  FixedArray(const T& defaultVal) {
+  constexpr FixedArray(const T& defaultVal) {
     for (size_t i = 0; i < size_; i++) {
       data[i] = defaultVal;
     }
   }
 
   template <typename... Args>
-  FixedArray(Args... args) {
+  constexpr FixedArray(Args... args) {
     initializeFromPack<0>(args...);
   }
 
-  size_t size() const { return size_; }
+  constexpr size_t size() const { return size_; }
 
-  T& at(size_t i) { return data[i]; }
-  const T& at(size_t i) const { return data[i]; }
+  constexpr T& at(size_t i) { return data[i]; }
+  constexpr const T& at(size_t i) const { return data[i]; }
 
-  T& operator[](size_t i) { return data[i]; }
-  const T& operator[](size_t i) const { return data[i]; }
+  constexpr T& operator[](size_t i) { return data[i]; }
+  constexpr const T& operator[](size_t i) const { return data[i]; }
 
-  iterator_t begin() { return iterator_t{*this, 0}; }
-  const_iterator_t begin() const { return const_iterator_t{*this, 0}; }
+  constexpr iterator_t begin() { return iterator_t{*this, 0}; }
+  constexpr const_iterator_t begin() const {
+    return const_iterator_t{*this, 0};
+  }
 
-  iterator_t end() { return iterator_t{*this, size_}; }
-  const_iterator_t end() const { return const_iterator_t{*this, size_}; }
+  constexpr iterator_t end() { return iterator_t{*this, size_}; }
+  constexpr const_iterator_t end() const {
+    return const_iterator_t{*this, size_};
+  }
 
-  operator Span<T>() { return Span<T>{data, data + size_}; }
-  operator Span<T const>() const { return Span<T const>{data, data + size_}; }
+  constexpr operator Span<T>() { return Span<T>{data, data + size_}; }
+  constexpr operator Span<T const>() const {
+    return Span<T const>{data, data + size_};
+  }
 
  private:
   template <size_t i, typename Arg, typename... Args>
-  void initializeFromPack(Arg a, Args... args) {
+  constexpr void initializeFromPack(Arg a, Args... args) {
     static_assert(
         i < size_, "Too many arguments used to initialize FixedArray");
     data[i] = a;
@@ -77,7 +84,7 @@ class FixedArray {
   }
 
   template <size_t i>
-  void initializeFromPack() {
+  constexpr void initializeFromPack() {
     static_assert(
         i == size_, "Too few arguments used to initialize FixedArray");
   }
@@ -94,28 +101,28 @@ class FixedArray<T, 0> {
    public:
     using value_t = IT;
 
-    bool operator==(NullIterator const& other) { return true; }
-    bool operator!=(NullIterator const& other) { return false; }
+    constexpr bool operator==(NullIterator const& other) { return true; }
+    constexpr bool operator!=(NullIterator const& other) { return false; }
 
-    IT& operator*() const { __builtin_unreachable(); }
+    constexpr IT& operator*() const { __builtin_unreachable(); }
 
-    NullIterator const& operator++() { return *this; }
+    constexpr NullIterator const& operator++() { return *this; }
   };
 
  public:
   using iterator_t = NullIterator<T>;
   using const_iterator_t = NullIterator<const T>;
 
-  size_t size() const { return 0; }
+  constexpr size_t size() const { return 0; }
 
-  iterator_t begin() { return iterator_t{}; }
-  iterator_t end() { return iterator_t{}; }
+  constexpr iterator_t begin() { return iterator_t{}; }
+  constexpr iterator_t end() { return iterator_t{}; }
 
-  const_iterator_t begin() const { return const_iterator_t{}; }
-  const_iterator_t end() const { return const_iterator_t{}; }
+  constexpr const_iterator_t begin() const { return const_iterator_t{}; }
+  constexpr const_iterator_t end() const { return const_iterator_t{}; }
 
-  operator Span<T>() { return Span<T>{}; }
-  operator Span<T const>() const { return Span<T const>{}; }
+  constexpr operator Span<T>() { return Span<T>{}; }
+  constexpr operator Span<T const>() const { return Span<T const>{}; }
 };
 
 } // namespace mysty

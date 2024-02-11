46ad469c6e5c53023e7e874f163adb81b5e6e48f
Matthew Hillman <mhillman2@aol.com>
2024-02-11 11:43:12 +0000

    Add `int_limits` template utility

--- src/bootloader/stage3/mysty/int.hpp
+++ src/bootloader/stage3/mysty/int.hpp
@@ -19,3 +19,36 @@ using size_t = __SIZE_TYPE__;
 #else
 using size_t = unsigned int;
 #endif
+
+template <typename T>
+struct int_limits;
+
+template <>
+struct int_limits<uint8_t> {
+  static constexpr uint8_t min = 0;
+  static constexpr uint8_t max = 0xFF;
+};
+
+template <>
+struct int_limits<uint16_t> {
+  static constexpr uint16_t min = 0;
+  static constexpr uint16_t max = 0xFFFF;
+};
+
+template <>
+struct int_limits<unsigned long> {
+  static constexpr unsigned long min = 0;
+  static constexpr unsigned long max = 0xFFFFFFFF;
+};
+
+template <>
+struct int_limits<unsigned int> {
+  static constexpr unsigned int min = 0;
+  static constexpr unsigned int max = 0xFFFFFFFF;
+};
+
+template <>
+struct int_limits<uint64_t> {
+  static constexpr uint64_t min = 0;
+  static constexpr uint64_t max = 0xFFFFFFFFFFFFFFFF;
+};

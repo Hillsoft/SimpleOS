d7532e7f5933cf24bcf87bdeebf86adb14b0b44a
Matthew Hillman <mhillman2@aol.com>
2024-02-11 11:44:05 +0000

    Additional utility methods for `Span`

--- src/bootloader/stage3/mysty/span.hpp
+++ src/bootloader/stage3/mysty/span.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
 #include "mysty/concepts/iterable.hpp"
+#include "mysty/concepts/orderable.hpp"
 #include "mysty/int.hpp"
+#include "mysty/optional.hpp"
 #include "mysty/string.hpp"
 #include "mysty/typeattributes.hpp"
 
@@ -53,6 +55,38 @@ class Span {
   constexpr iterator_t end() const { return iterator_t{end_}; }
 
   constexpr size_t size() const { return static_cast<size_t>(end_ - start_); }
+
+  constexpr Span slice_front(
+      size_t offset, size_t count = int_limits<size_t>::max) const {
+    size_t actualCount = min(count, size() - offset);
+    return Span{start_ + offset, actualCount};
+  }
+
+  template <typename T2 = T>
+    requires(HasEquality<T>)
+  constexpr bool operator==(const Span& other) const {
+    if (other.size() != size()) {
+      return false;
+    }
+    for (size_t i = 0; i < size(); ++i) {
+      if (at(i) != other.at(i)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  template <typename T2 = T>
+    requires(HasEquality<T>)
+  constexpr Optional<size_t> find(const T& needle) const {
+    for (size_t i = 0; i < size(); ++i) {
+      if (at(i) == needle) {
+        return i;
+      }
+    }
+    return {};
+  }
 
  protected:
   T* start_;

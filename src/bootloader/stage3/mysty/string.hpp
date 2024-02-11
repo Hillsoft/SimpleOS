fd856d856cd7fa0ebbdfa8278ec882bd59795492
Matthew Hillman <mhillman2@aol.com>
2024-02-11 19:41:31 +0000

    Optimise strlen

--- src/bootloader/stage3/mysty/string.hpp
+++ src/bootloader/stage3/mysty/string.hpp
@@ -4,11 +4,10 @@
 
 namespace mysty {
 
-constexpr size_t strlen(const char* str) {
+constexpr size_t strlen(const char* str) noexcept {
   size_t len = 0;
-  while (*str) {
+  while (str[len] != 0) {
     ++len;
-    ++str;
   }
   return len;
 }

8f62da1360d48142375665dfd4c89e3b5854d0f2
Matthew Hillman <mhillman2@aol.com>
2024-02-15 18:21:55 +0000

    Remove redundant function signatures

--- src/bootloader/stage3/x86.hpp
+++ src/bootloader/stage3/x86.hpp
@@ -3,9 +3,6 @@
 #include "mysty/int.hpp"
 
 extern "C" {
-// void __attribute__((cdecl)) x86_outb(uint16_t port, uint8_t value);
-// uint8_t __attribute__((cdecl)) x86_inb(uint16_t port);
-
 void __attribute__((cdecl)) x86_load_interrupt_table(void* table);
 void __attribute__((cdecl)) x86_trivial_interrupt(void* interruptFrame);
 }

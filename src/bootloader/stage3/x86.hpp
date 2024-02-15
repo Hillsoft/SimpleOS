3d2e4a836015f15105ff99d3db9ae374cdcfdf48
Matthew Hillman <mhillman2@aol.com>
2024-02-15 18:19:18 +0000

    Move `in` and `out` assembly to C++ for better inlining

--- src/bootloader/stage3/x86.hpp
+++ src/bootloader/stage3/x86.hpp
@@ -3,11 +3,25 @@
 #include "mysty/int.hpp"
 
 extern "C" {
-void __attribute__((cdecl)) x86_outb(uint16_t port, uint8_t value);
-uint8_t __attribute__((cdecl)) x86_inb(uint16_t port);
+// void __attribute__((cdecl)) x86_outb(uint16_t port, uint8_t value);
+// uint8_t __attribute__((cdecl)) x86_inb(uint16_t port);
 
 void __attribute__((cdecl)) x86_load_interrupt_table(void* table);
 void __attribute__((cdecl)) x86_trivial_interrupt(void* interruptFrame);
+}
+
+inline void x86_outb(uint16_t port, uint8_t value) {
+  asm volatile("out %b[value], %w[port]"
+               : /* no output */
+               : [port] "d"(port), [value] "a"(value));
+}
+
+inline uint8_t x86_inb(uint16_t port) {
+  uint8_t result = 0;
+  asm volatile("in %w[port], %b[result] "
+               : [result] "+a"(result)
+               : [port] "d"(port));
+  return result;
 }
 
 inline void x86_io_wait() {

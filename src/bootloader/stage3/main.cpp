b30f889e9365031fc3aa4c99955bbed84ae12784
Matthew Hillman <mhillman2@aol.com>
2024-08-02 21:31:27 +0100

    Run event loop after initialisation

--- src/bootloader/stage3/main.cpp
+++ src/bootloader/stage3/main.cpp
@@ -1,7 +1,9 @@
 #include "main.hpp"
 
 #include "fat.hpp"
+#include "globaleventqueue.hpp"
 #include "init.hpp"
+#include "interrupts.hpp"
 #include "mysty/array.hpp"
 #include "mysty/int.hpp"
 #include "mysty/io.hpp"
@@ -79,8 +81,8 @@ void testString() {
 
 extern "C" {
 
-void __attribute__((cdecl)) __attribute__((externally_visible))
-cstart(uint8_t bootDrive) {
+void __attribute__((cdecl)) __attribute__((externally_visible)) cstart(
+    uint8_t bootDrive) {
   if (!simpleos::initialize(bootDrive)) {
     constexpr mysty::StringView errorMessage{
         "Failed to initialise, aborting!\n"};
@@ -91,5 +93,13 @@ cstart(uint8_t bootDrive) {
   // testFile();
   // testMemory();
   // testString();
+
+  while (true) {
+    if (simpleos::areEventsWaiting()) {
+      simpleos::dispatchNextEvent();
+    } else {
+      simpleos::awaitInterrupt();
+    }
+  }
 }
 }

7b92aee1cdb97cb512f26ebed1d0d0b138253ed3
Matthew Hillman <mhillman2@aol.com>
2024-08-04 17:49:28 +0100

    Add keyboard event handler

--- src/bootloader/stage3/terminal.cpp
+++ src/bootloader/stage3/terminal.cpp
@@ -1,14 +1,35 @@
 #include "terminal.hpp"
 
+#include "globaleventqueue.hpp"
+#include "hid/keyboard.hpp"
 #include "mysty/io.hpp"
 #include "mysty/span.hpp"
+#include "mysty/storage.hpp"
 
 namespace simpleos {
+
+namespace {
+
+class KeyboardEventHandler : public EventHandler<hid::KeyboardEvent> {
+ public:
+  void handleEvent(hid::KeyboardEvent event) override {
+    if (event.ascii > 0 && event.type == hid::KeyboardEvent::Type::PRESS) {
+      mysty::putc(event.ascii);
+    }
+  }
+};
+
+mysty::StorageFor<KeyboardEventHandler> eventHandler;
+
+} // namespace
 
 void startTerminal() {
   mysty::clrscr();
   constexpr mysty::StringView kWelcomeMessage = "Welcome to SimpleOS!\n\n\n";
   mysty::puts(kWelcomeMessage);
+
+  eventHandler.emplace();
+  registerHandler(&*eventHandler);
 }
 
 } // namespace simpleos

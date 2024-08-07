618e00964a86c2d75ecbb403828e6c3a0c58a2aa
Matthew Hillman <mhillman2@aol.com>
2024-08-07 23:07:15 +0100

    Add 'cat' command to print file contents to the console

--- src/bootloader/stage3/terminal/commandregistry.cpp
+++ src/bootloader/stage3/terminal/commandregistry.cpp
@@ -1,5 +1,7 @@
 #include "terminal/commandregistry.hpp"
 
+#include "fat.hpp"
+#include "mysty/array.hpp"
 #include "mysty/io.hpp"
 
 namespace simpleos {
@@ -14,11 +16,49 @@ void echoCommand(mysty::Span<const mysty::StringView> args) {
   }
 }
 
+void catCommand(mysty::Span<const mysty::StringView> args) {
+  if (args.size() < 2) {
+    constexpr mysty::StringView kTooFewArgs = "Usage: cat <file_name>";
+    mysty::puts(kTooFewArgs);
+  }
+
+  mysty::StringView fileName = args[1];
+
+  mysty::Optional<File> file = openFile(fileName);
+  if (!file.has_value()) {
+    constexpr mysty::StringView errorMessage{"Failed to open 'test.txt'\n"};
+    mysty::puts(errorMessage);
+    return;
+  }
+
+  mysty::FixedArray<uint8_t, 100> readBuffer;
+  File::ReadResult result = File::ReadResult::OK;
+
+  while (result == File::ReadResult::OK) {
+    size_t remaining = file->remainingBytes();
+
+    result = file->read(readBuffer);
+
+    if (result == File::ReadResult::FAILED) {
+      constexpr mysty::StringView errorMessage{"\nFailed to read file\n"};
+      mysty::puts(errorMessage);
+      return;
+    }
+
+    mysty::puts(static_cast<mysty::Span<uint8_t>>(readBuffer)
+                    .reinterpret<const char>()
+                    .slice_front(0, remaining));
+  }
+}
+
 } // namespace
 
 mysty::Optional<TerminalCommand> getCommand(mysty::StringView commandName) {
   if (commandName == "echo") {
     return echoCommand;
+  }
+  if (commandName == "cat") {
+    return catCommand;
   }
 
   return mysty::Optional<TerminalCommand>{};

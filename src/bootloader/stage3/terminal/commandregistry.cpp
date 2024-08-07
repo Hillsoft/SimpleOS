f5e25c0bfb5d7e38c7b1fdd82dfa0e0dbe4abdc6
Matthew Hillman <mhillman2@aol.com>
2024-08-07 23:01:46 +0100

    Add basic echo command

--- src/bootloader/stage3/terminal/commandregistry.cpp
+++ src/bootloader/stage3/terminal/commandregistry.cpp
@@ -1,8 +1,26 @@
 #include "terminal/commandregistry.hpp"
 
+#include "mysty/io.hpp"
+
 namespace simpleos {
 
+namespace {
+
+void echoCommand(mysty::Span<const mysty::StringView> args) {
+  args = args.slice_front(1);
+  for (const auto& a : args) {
+    mysty::puts(a);
+    mysty::putc(' ');
+  }
+}
+
+} // namespace
+
 mysty::Optional<TerminalCommand> getCommand(mysty::StringView commandName) {
+  if (commandName == "echo") {
+    return echoCommand;
+  }
+
   return mysty::Optional<TerminalCommand>{};
 }
 

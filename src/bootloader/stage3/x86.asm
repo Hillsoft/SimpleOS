2dc29c8de268732bfaabef31b1cdf0aca9963e0d
Matthew Hillman <mhillman2@aol.com>
2024-08-09 23:59:08 +0100

    Update assembly in x86.asm to System V ABI and 64-bit opcodes

--- src/bootloader/stage3/x86.asm
+++ src/bootloader/stage3/x86.asm
@@ -1,16 +1,15 @@
+bits 64
+
 global x86_load_interrupt_table
 x86_load_interrupt_table:
-    [bits 32]
-    mov eax, [esp + 4]
-    lidt [eax]
+    lidt [rdi]
     ret
 
 global x86_trivial_interrupt
 align 4
 x86_trivial_interrupt:
-    [bits 32]
-    push eax
+    push rax
     mov al, 20h
     out 20h, al
-    pop eax
-    iret
+    pop rax
+    iretq

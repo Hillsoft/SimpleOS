aafb4351c8f6b033400ad9e9a3b116a482a5cf1d
Matthew Hillman <mhillman2@aol.com>
2024-08-09 23:13:34 +0100

    Enable SSE2 extensions

--- src/bootloader/stage3/after_protected.asm
+++ src/bootloader/stage3/after_protected.asm
@@ -60,6 +60,15 @@ pstart:
   or eax, 1 << 31
   mov cr0, eax
 
+  ; enable SSE
+  mov eax, cr0
+  and ax, 0xFFFB
+  or ax, 0x2
+  mov cr0, eax
+  mov eax, cr4
+  or ax, 3 << 9
+  mov cr4, eax
+
   ; load 64-bit GDT
   lgdt [GDT.Pointer]
   mov dl, [boot_drive]

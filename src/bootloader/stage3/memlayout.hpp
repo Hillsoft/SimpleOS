5068a7298e9b52a15a444e09cf789141cb9dda60
Matthew Hillman <mhillman2@aol.com>
2024-08-11 22:13:47 +0100

    Update FAT data arena to avoid overlapping the page table

--- src/bootloader/stage3/memlayout.hpp
+++ src/bootloader/stage3/memlayout.hpp
@@ -4,7 +4,7 @@ namespace simpleos {
 
 inline void* kMemMapSize = reinterpret_cast<void*>(0x500);
 inline void* kMemMapStart = reinterpret_cast<void*>(0x508);
-inline void* kFatArenaStart = reinterpret_cast<void*>(0x1000);
+inline void* kFatArenaStart = reinterpret_cast<void*>(0x5000);
 inline void* kFatArenaEnd = reinterpret_cast<void*>(0x11000);
 
 } // namespace simpleos

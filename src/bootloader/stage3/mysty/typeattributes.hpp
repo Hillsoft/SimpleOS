f7de7dfd419b97cb72140403375e23945df3c1f4
Matthew Hillman <mhillman2@aol.com>
2024-08-12 20:36:10 +0100

    Add integral and pointer type attributes

--- src/bootloader/stage3/mysty/typeattributes.hpp
+++ src/bootloader/stage3/mysty/typeattributes.hpp
@@ -74,4 +74,85 @@ concept non_trivially_destructible = !trivially_destructible<T>;
 template <typename T, typename U>
 concept is_base_of = __is_base_of(T, U);
 
+template <typename T>
+struct is_integral_t {
+  constexpr static bool value = false;
+};
+
+template <>
+struct is_integral_t<bool> {
+  constexpr static bool value = true;
+};
+
+template <>
+struct is_integral_t<char> {
+  constexpr static bool value = true;
+};
+
+template <>
+struct is_integral_t<unsigned char> {
+  constexpr static bool value = true;
+};
+
+template <>
+struct is_integral_t<signed char> {
+  constexpr static bool value = true;
+};
+
+template <>
+struct is_integral_t<signed short> {
+  constexpr static bool value = true;
+};
+
+template <>
+struct is_integral_t<unsigned short> {
+  constexpr static bool value = true;
+};
+
+template <>
+struct is_integral_t<signed int> {
+  constexpr static bool value = true;
+};
+
+template <>
+struct is_integral_t<unsigned int> {
+  constexpr static bool value = true;
+};
+
+template <>
+struct is_integral_t<signed long> {
+  constexpr static bool value = true;
+};
+
+template <>
+struct is_integral_t<unsigned long> {
+  constexpr static bool value = true;
+};
+
+template <>
+struct is_integral_t<signed long long> {
+  constexpr static bool value = true;
+};
+
+template <>
+struct is_integral_t<unsigned long long> {
+  constexpr static bool value = true;
+};
+
+template <typename T>
+concept integral = is_integral_t<T>::value;
+
+template <typename T>
+struct is_pointer_t {
+  constexpr static bool value = false;
+};
+
+template <typename T>
+struct is_pointer_t<T*> {
+  constexpr static bool value = true;
+};
+
+template <typename T>
+concept pointer = is_pointer_t<T>::value;
+
 } // namespace mysty

f6041dab90a8480b787cc300f55177bb760125d6
Matthew Hillman <mhillman2@aol.com>
2024-08-12 20:59:22 +0100

    Make circular buffer use atomics, it now supports single reader + single writer concurrency; which is exactly what we need for its use in the current interrupt handlers

--- src/bootloader/stage3/mysty/circularBuffer.hpp
+++ src/bootloader/stage3/mysty/circularBuffer.hpp
@@ -1,11 +1,16 @@
 #pragma once
 
 #include "mysty/array.hpp"
+#include "mysty/atomic.hpp"
 #include "mysty/storage.hpp"
 #include "mysty/typeattributes.hpp"
 
 namespace mysty {
 
+/*
+ * Supports single reader + single writer concurrency.
+ * clear() cannot be called concurrently with any other access
+ */
 template <typename T, size_t size_>
   requires is_not_const<T>
 class FixedCircularBuffer {
@@ -24,40 +29,59 @@ class FixedCircularBuffer {
 
   void clear() {
     if constexpr (non_trivially_destructible<T>) {
-      for (size_t i = startIndex_; i != endIndex_; i = nextIndex(i)) {
+      size_t curEnd = endIndex_.load(MemoryOrder::Acquire);
+      for (size_t i = startIndex_.load(MemoryOrder::Acquire); i != curEnd;
+           i = nextIndex(i)) {
         data_[i].destroy();
       }
     }
-    startIndex_ = 0;
-    endIndex_ = 0;
+    startIndex_.store(0, mysty::MemoryOrder::Release);
+    endIndex_.store(0, mysty::MemoryOrder::Release);
+  }
+
+  bool empty() const {
+    return startIndex_.load(MemoryOrder::Relaxed) ==
+        endIndex_.load(MemoryOrder::Relaxed);
   }
 
   size_t size() const {
-    if (endIndex_ >= startIndex_) {
-      return endIndex_ - startIndex_;
+    size_t curStart = startIndex_.load(MemoryOrder::Relaxed);
+    size_t curEnd = endIndex_.load(MemoryOrder::Relaxed);
+    if (curEnd >= curStart) {
+      return curEnd - curStart;
     } else {
-      return size_ - startIndex_ + endIndex_;
+      return size_ - curStart + curEnd;
     }
   }
 
   template <typename... Args>
   void emplace_back(Args... args) {
-    if (nextIndex(endIndex_) != startIndex_) {
-      data_[endIndex_].emplace(mysty::forward<Args...>(args)...);
-      endIndex_ = nextIndex(endIndex_);
+    size_t curStart = startIndex_.load(MemoryOrder::Acquire);
+    size_t curEnd = endIndex_.load(MemoryOrder::Acquire);
+    if (nextIndex(curEnd) != curStart) {
+      data_[curEnd].emplace(mysty::forward<Args...>(args)...);
+      endIndex_.store(nextIndex(curEnd), MemoryOrder::Release);
     }
   }
 
-  T& peek_front() { return *data_[startIndex_]; }
-  const T& peek_front() const { return *data_[startIndex_]; }
+  T& peek_front() {
+    endIndex_.load(MemoryOrder::Acquire);
+    return *data_[startIndex_.load(MemoryOrder::Acquire)];
+  }
+  const T& peek_front() const {
+    endIndex_.load(MemoryOrder::Acquire);
+    return *data_[startIndex_.load(MemoryOrder::Acquire)];
+  }
 
   T pop_front() {
-    StorageFor<T>& front = data_[startIndex_];
+    endIndex_.load(MemoryOrder::Acquire);
+    size_t curStart = startIndex_.load(MemoryOrder::Acquire);
+    StorageFor<T>& front = data_[curStart];
     T result = mysty::move(*front);
     if constexpr (non_trivially_destructible<T>) {
       front.destroy();
     }
-    startIndex_ = nextIndex(startIndex_);
+    startIndex_.store(nextIndex(curStart), MemoryOrder::Release);
     return result;
   }
 
@@ -71,8 +95,8 @@ class FixedCircularBuffer {
   }
 
   FixedArray<StorageFor<T>, size_> data_;
-  size_t startIndex_ = 0;
-  size_t endIndex_ = 0;
+  mysty::Atomic<size_t> startIndex_ = 0;
+  mysty::Atomic<size_t> endIndex_ = 0;
 };
 
 } // namespace mysty
